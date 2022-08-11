e1b593d8e711e9371ea6704c2e82e8a26d898ed8
Marco Neumann <marco@crepererum.net>
2022-08-11 16:09:00 +0200

    rename to otel2chrome

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-podman build -t otlp-flamegraph .
+podman build -t otel2chrome .
 docker-compose up &
 
 read -p "Press enter to continue"
 
-podman cp otlp-flamegraph-otel-collector-1:/data/otel.json data/otel.json
+podman cp otel2chrome-otel2chrome1:/data/otel.json data/otel.json
 docker-compose down -v
 
 ./convert.py > data/out.json
